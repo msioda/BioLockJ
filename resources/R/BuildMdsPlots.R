66a3f983e4d3ee4ee9387def8668e5008f3bddf3
Ivory Blakley <ieclabau@uncc.edu>
2019-01-11 00:14:25 -0500

    Cleanup unused code, tidy saved tabled, correct coloring by factors.

--- resources/R/BuildMdsPlots.R
+++ resources/R/BuildMdsPlots.R
@@ -8,6 +8,27 @@ main <- function() {
 	mdsAtts = getProperty( "rMds.reportFields", c( getBinaryFields(), getNominalFields() )  )
 	for( otuLevel in getProperty("report.taxonomyLevels") ) {
 		if( doDebug() ) sink( file.path( getModuleDir(), "temp", paste0("debug_BuildMdsPlots_", otuLevel, ".log") ) )
+		# get input data
+		inputFile = getPipelineFile( paste0(otuLevel, ".*_metaMerged.tsv") )
+		if( doDebug() ) print( paste( "inputFile = list.files( tableDir, paste0(otuLevel, .*, _metaMerged.tsv), full.names=TRUE ):", inputFile ) )
+		if( length( inputFile ) == 0 ) { next }
+		otuTable = read.table( inputFile, check.names=FALSE, na.strings=getProperty("metadata.nullValue", "NA"), 
+													 comment.char=getProperty("metadata.commentChar", ""), header=TRUE, sep="\t", row.names=1 )
+		lastOtuCol = ncol(otuTable) - getProperty("internal.numMetaCols")
+		#
+		# calculate PCoA
+		myMDS = capscale( otuTable[,1:lastOtuCol]~1, distance=getProperty("rMds.distance") )
+		#
+		# save PCoA table
+		pcoaFileName = paste0( getPath( file.path(getModuleDir(), "temp"), paste0(otuLevel, "_pcoa") ), ".tsv" )
+		if( doDebug() ) print( paste( "Saving PCoA table to ", pcoaFileName ) )
+		write.table( cbind(id=row.names(myMDS$CA$u), as.data.frame(myMDS$CA$u)), file=pcoaFileName, col.names=TRUE, row.names=FALSE, sep="\t" )
+		# save eigen values
+		eigenFileName = paste0( getPath( file.path(getModuleDir(), "temp"), paste0(otuLevel, "_eigenValues") ), ".tsv" )
+		if( doDebug() ) print( paste( "Saving PCoA table to ", pcoaFileName ) )
+		write.table( data.frame(mds=names(myMDS$CA$eig), eig=myMDS$CA$eig), file=eigenFileName, col.names=FALSE, row.names=FALSE, sep="\t")
+		#
+		# Make plots
 		outputFile = paste0( getPath( file.path(getModuleDir(), "output"), paste0(otuLevel, "_MDS.pdf" ) ) )
 		if( doDebug() ) print( paste( "Saving plots to ", outputFile ) )
 		if (numAxis < 4 ) {
@@ -18,41 +39,26 @@ main <- function() {
 			par( mfrow=c(3, 2) )
 		}
 		par(las=1, oma=c(0,1,4,0), mar=c(5, 4, 2, 2))
-		inputFile = getPipelineFile( paste0(otuLevel, ".*_metaMerged.tsv") )
-		if( doDebug() ) print( paste( "inputFile = list.files( tableDir, paste0(otuLevel, .*, _metaMerged.tsv), full.names=TRUE ):", inputFile ) )
-		if( length( inputFile ) == 0 ) { next }
-		otuTable = read.table( inputFile, check.names=FALSE, na.strings=getProperty("metadata.nullValue", "NA"), 
-													 comment.char=getProperty("metadata.commentChar", ""), header=TRUE, sep="\t", row.names=1 )
-		mdsCols = getColIndexes( otuTable, mdsAtts )
-		lastOtuCol = ncol(otuTable) - getProperty("internal.numMetaCols")
-		myMDS = capscale( otuTable[,1:lastOtuCol]~1, distance=getProperty("rMds.distance") )
-		pcoaFileName = paste0( getPath( file.path(getModuleDir(), "temp"), paste0(otuLevel, "_pcoa") ), ".tsv" )
-		if( doDebug() ) print( paste( "Saving table to ", pcoaFileName ) )
-		write.table( myMDS$CA$u, file=pcoaFileName, sep="\t" )
-		eigenFileName = paste0( getPath( file.path(getModuleDir(), "temp"), paste0(otuLevel, "_eigenValues") ), ".tsv" )
-		write.table( myMDS$CA$eig, file=eigenFileName, sep="\t")
 		percentVariance = as.numeric(eigenvals(myMDS)/sum( eigenvals(myMDS) ) ) * 100
-		colors = getColors(2)
-		for( metaCol in mdsCols ){
-			metaColVals = otuTable[,metaCol]
+		for (attName in mdsAtts){
+			metaColVals = as.character(otuTable[,attName])
 			par(mfrow = par("mfrow"))
 			att = as.factor(metaColVals)
-			attName = names(otuTable)[metaCol]
-			vals = levels( att )
-			colorKey = getMdsColors( otuTable, metaCol )
+			colorKey = getMdsColorKey( metaColVals )
 			position = 1
 			page = 1
+			numAxis = min(c(numAxis, ncol(myMDS$CA$u)))
 			for (x in 1:(numAxis-1)) {
 				for (y in (x+1):numAxis) {
 					plot( myMDS$CA$u[,x], myMDS$CA$u[,y], main=paste(y, "vs", x),
 								xlab=getMdsLabel( x, percentVariance[x] ),
 								ylab=getMdsLabel( y, percentVariance[y] ),
-								cex=1.2, pch=getProperty("r.pch"), col=colorKey[otuTable[,metaCol]] )
+								cex=1.2, pch=getProperty("r.pch"), col=colorKey[metaColVals] )
 					position = position + 1
 					if ( position == 2 ){ 
 						# put this plot at the upper right position
 						# that puts the legend in a nice white space, and it makes axis 1 in line with itself in two plots (same for axis3)
-						plotRelativeVariance(percentVariance)
+						plotRelativeVariance(percentVariance, numAxis)
 						legend(x="topright", title=attName,
 									 legend = paste0(names(colorKey), " (n=", table(metaColVals)[names(colorKey)], ")"), 
 									 col=colorKey, pch=getProperty("r.pch"), bty="n")
@@ -79,8 +85,8 @@ getMdsLabel <- function( axisNum, variance ) {
 	return( paste0("Axis ", axisNum, " (", paste0( round( variance ), "%)" ) ) )
 }
 
-getMdsColors <- function( otuTable,  metaCol ) { 
-	tableVals = as.factor( otuTable[,metaCol] )
+getMdsColorKey <- function( metaColVals ) { 
+	tableVals = as.factor( metaColVals )
 	factors = levels( tableVals )
 	colors = getColors( length( factors ) )
 	names(colors) = levels( tableVals )
@@ -88,9 +94,9 @@ getMdsColors <- function( otuTable,  metaCol ) {
 }
 
 
-plotRelativeVariance <- function(percentVariance){
+plotRelativeVariance <- function(percentVariance, numAxis){
 	numBars = min(c(length(percentVariance), 6)) # arbitrary choice, don't show more than 6
-	numBars = max(numBars, getProperty("rMds.numAxis"))
+	numBars = max(numBars, numAxis)
 	heights = percentVariance[1:numBars]
 	bp = barplot(heights, col="dodgerblue1", ylim=c(0,100), names=1:numBars,
 							 xlab="axis", ylab="variance explained per axis", main="")
