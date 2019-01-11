7989f7422a467ec91cde609e5aea3ae28b8f0c3d
Ivory Blakley <ieclabau@uncc.edu>
2019-01-10 23:22:45 -0500

    improve page layout

--- resources/R/BuildMdsPlots.R
+++ resources/R/BuildMdsPlots.R
@@ -11,10 +11,10 @@ main <- function() {
 		outputFile = paste0( getPath( file.path(getModuleDir(), "output"), paste0(otuLevel, "_MDS.pdf" ) ) )
 		if( doDebug() ) print( paste( "Saving plots to ", outputFile ) )
 		if (numAxis < 4 ) {
-			pdf( outputFile )
+			pdf( outputFile, width = 7, height = 7)
 			par( mfrow=c(2, 2) )
 		}else{
-			pdf( outputFile, paper="letter" )
+			pdf( outputFile, paper="letter", width=7, height=10.5 )
 			par( mfrow=c(3, 2) )
 		}
 		par(las=1, oma=c(0,1,4,0), mar=c(5, 4, 2, 2))
@@ -34,30 +34,42 @@ main <- function() {
 		percentVariance = as.numeric(eigenvals(myMDS)/sum( eigenvals(myMDS) ) ) * 100
 		colors = getColors(2)
 		for( metaCol in mdsCols ){
+			metaColVals = otuTable[,metaCol]
 			par(mfrow = par("mfrow"))
-			att = as.factor(otuTable[,metaCol])
+			att = as.factor(metaColVals)
 			attName = names(otuTable)[metaCol]
 			vals = levels( att )
 			colorKey = getMdsColors( otuTable, metaCol )
 			position = 1
+			page = 1
 			for (x in 1:(numAxis-1)) {
 				for (y in (x+1):numAxis) {
-					if (position == 2){ 
-						# put this plot at the upper right position
-						# that puts the legend in a nice white space, and it makes axis 1 in line with itself in two plots (same for axis3)
-						plotRelativeVariance(percentVariance)
-						legend(x="topright", legend = names(colorKey), col=colorKey, pch=getProperty("r.pch"), bty="n")
-						}
 					plot( myMDS$CA$u[,x], myMDS$CA$u[,y], main=paste(y, "vs", x),
 								xlab=getMdsLabel( x, percentVariance[x] ),
 								ylab=getMdsLabel( y, percentVariance[y] ),
 								cex=1.2, pch=getProperty("r.pch"), col=colorKey[otuTable[,metaCol]] )
 					position = position + 1
+					if ( position == 2 ){ 
+						# put this plot at the upper right position
+						# that puts the legend in a nice white space, and it makes axis 1 in line with itself in two plots (same for axis3)
+						plotRelativeVariance(percentVariance)
+						legend(x="topright", title=attName,
+									 legend = paste0(names(colorKey), " (n=", table(metaColVals)[names(colorKey)], ")"), 
+									 col=colorKey, pch=getProperty("r.pch"), bty="n")
+						# this is the best time to add the page title
+						title(main="Multidimensional Scaling Plots", outer = TRUE, line=1.2, cex=1.5)
+						titlePart2 = ifelse( page == 1, paste( "taxonomic level:", otuLevel), paste("page", page))
+						title(main=titlePart2, outer = TRUE, line=0, cex=.5)
+						position = position + 1
+					}
+					if (position > prod(par("mfrow") ) ) {
+						position = 1
+						page = page + 1
+					}
 				}
 			}
-			title(main=paste( "MDS", attName), outer = TRUE, line=1.2, cex=1.5)
-			title(main=paste( "taxonomic level:", otuLevel), outer = TRUE, line=0, cex=.5)
 		}
+		plotPlainText(paste("Each set of", page, ifelse(page==1, "page", "pages"), "is identical.\nOnly the color scheme/legend changes."))
 		if( doDebug() ) sink()
 		dev.off()
 	}
@@ -78,9 +90,9 @@ getMdsColors <- function( otuTable,  metaCol ) {
 
 plotRelativeVariance <- function(percentVariance){
 	numBars = min(c(length(percentVariance), 6)) # arbitrary choice, don't show more than 6
+	numBars = max(numBars, getProperty("rMds.numAxis"))
 	heights = percentVariance[1:numBars]
-	bp = barplot(heights, col="dodgerblue1", ylim=c(0,100),
+	bp = barplot(heights, col="dodgerblue1", ylim=c(0,100), names=1:numBars,
 							 xlab="axis", ylab="variance explained per axis", main="")
-	mtext(at=bp, line=1, text=1:numBars, side=1)
 	text(x=bp, y=heights, labels = paste(round(heights), "%"), pos=3, xpd=TRUE)
 }
