16714ee813c501955bb5e823218e6ce03e8f49d5
Aaron_Y <amyerke@uncc.edu>
2019-01-22 15:56:08 -0500

    Created layout.js to hold common functions between config_config.js and splash.js.  Created returnPromiseFromServer() that can be recycled by other functions and wrapped by such functions as retrievePropertiesFile(), retrieveProjects, ect...

---
+++ web_app/public/javascripts/layout.js
@@ -0,0 +1,65 @@
+function saveConfigToGui(configTextandNameObject){
+  var request = new XMLHttpRequest();
+  request.open('POST', '/saveConfigToGui', true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(
+    configTextandNameObject
+  ));
+  console.log('launch sent');
+  request.onreadystatechange = function() {
+  if (request.readyState == XMLHttpRequest.DONE) {
+    console.log(request.responseText);
+    //window.location = '/progress';
+    }
+  }
+  //return returnPromiseFromServer('/saveConfigToGui', requestParameter = configTextandNameObject);
+}
+
+function retrieveProjects(){//gets all project names
+  return returnPromiseFromServer('/retrieveProjects', requestParameter = null);
+}//end function retrieveProjects
+
+function retrieveConfigs(){
+  return returnPromiseFromServer('/retrieveConfigs', requestParameter = null);
+}//end function retrieveProjects
+
+function retrievePropertiesFile(configName){
+  console.log(configName);
+  return returnPromiseFromServer('/retrievePropertiesFile', requestParameter = configName);
+}
+
+// function retreiveDefaultProps(dpropPath){
+//   returnPromiseFromServer('/defaultproperties', requestParameter = dpropPath)
+// }
+
+function returnPromiseFromServer(address, requestParameter, method = 'POST'){
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.open(method, address, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function() {
+      try{
+        if(this.status === 200 && request.readyState === 4){
+          resolve(JSON.parse(this.responseText));
+        }else{
+          reject(this.status + " " + this.statusText)
+        }
+      } catch(e) {
+        reject (e.message);
+      }
+    };
+    request.onerror = function() {
+      reject(this.status + " " + this.statusText)
+    };
+    try {
+      if (requestParameter) {
+        request.send(JSON.stringify(requestParameter));
+      }
+      else {
+        request.send();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  })
+}
