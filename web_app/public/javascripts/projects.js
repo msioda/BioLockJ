fac70e1463cbceb373d1b4c5db23194a43647f17
Aaron_Y <amyerke@uncc.edu>
2019-02-18 20:59:05 -0500

    pages for project manager gui

---
+++ web_app/public/javascripts/projects.js
@@ -0,0 +1,30 @@
+/**
+Author: Aaron Yerke
+Purpose: This holds the React.js to build the project manager GUI
+*/
+
+/**
+// TODO:
+Components needed for project manager:
+  -Manager/main-container
+    +Make a list of all projects
+      ^sorting options
+      ^pagination
+      ^list how many results/how many per page
+    -Project/sub-container
+      ^project
+      -Dataset-sub^2-container
+        -Pipeline container
+          Config file
+          Commandline Output
+          progress
+
+          Buttons:
+            Download All
+            Download reportFields
+            Go to visualizer
+
+// TODO:
+  postgres v11 database calls
+
+*/
