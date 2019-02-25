cb89c564aefe5f4605ad5eb7fe97bf436d0e7736
Aaron_Y <amyerke@uncc.edu>
2019-02-25 14:40:08 -0500

    script to aid in running Malcolm's AWS related scripts.

---
+++ web_app/DEV_ONLY_downloadMalcolmGitAddToPath.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/mjzapata/AWSBatchGenomicsStack.git
+echo 'export PATH=$PATH:/app/biolockj/webapp/AWSBatchGenomicsStack' >> ~/.bash_profile
+cat ~/.bash_profile
+source ~/.bash_profile
+echo $PATH
