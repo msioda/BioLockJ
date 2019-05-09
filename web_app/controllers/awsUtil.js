42a8d7bc534129e6cb62474a64f71729f593052b
Aaron_Y <amyerke@uncc.edu>
2019-05-09 15:52:08 -0400

    Update Notes.

--- web_app/controllers/awsUtil.js
+++ web_app/controllers/awsUtil.js
@@ -1,14 +1,12 @@
 /**
 Author: Aaron Yerke
 Purpose: Functions for AWS config properties related to AWS
-Notes:Notes, not ready for this yet...
+Notes: Backend server functions
 */
 
 function helloWorld(){
   console.log('%c hello world from config_aws', 'color: orange; font-weight: bold;');
 }
-
-helloWorld();
 
 exports.listAwsProfiles = function (req, res, next) {
   res.setHeader('Content-Type', 'text/html');
@@ -26,3 +24,17 @@ exports.listEc2InstanceIds = function (req, res, next) {
   res.write((JSON.stringify(['profile1', 'profile2', 'profile3'])));
   res.end();
 }
+
+// let aws = spawn(`source ${batchAwsConfigFile} ; launchEC2Node.sh ${req.body.formData.launchAction} ${req.body.formData.ec2StackName} ${req.body.formData.instanceName} ${req.body.formData.instanceType} ${req.body.formData.scriptName}`, {shell: '/bin/bash'});
+// // source ~/.batchawsdeploy/config ; launchEC2Node.sh directconnect testthis HeadNode t2.micro
+// aws.stdout.on('data', function (data) {
+//   console.log('stdout: ' + data.toString());
+// });
+//
+// aws.stderr.on('data', function (data) {
+//   console.log('stderr: ' + data.toString());
+// });
+//
+// aws.on('exit', function (code) {
+//   console.log('child process exited with code ' + code.toString());
+// });
