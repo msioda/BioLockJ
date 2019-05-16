5a1e7360b6b1d454d4fa79128f392afcc3dbe6fc
Aaron_Y <amyerke@uncc.edu>
2019-05-16 12:05:33 -0400

    Move javaDocs code from index.js to javaDocs.js.

---
+++ web_app/controllers/javaDocs.js
@@ -0,0 +1,25 @@
+/**
+Author: Aaron Yerke
+Purpose: Functions for reading the Java docs
+Notes: Backend server functions
+*/
+
+const path = require('path'),
+  fs = require('fs'),
+  bljDir = process.env.BLJ;
+
+exports.javaDocsModGetter =  function(req, res, next) {
+  try {
+    let modPathString = req.body.moduleJavaClassPath;
+    modPathString = modPathString.concat('.java');
+    const modPathArray = modPathString.split('/');
+    // console.log(path.join.apply(null, modPathArray));
+    const datum = fs.readFileSync(path.join(bljDir, 'src', path.join.apply(null, modPathArray)), 'utf8');
+      res.setHeader("Content-Type", "text/html");
+      res.write(datum);
+      res.end();
+  } catch (e) {
+    console.error(e);
+    errorLogger.writeError(e.stack);
+  }
+}
