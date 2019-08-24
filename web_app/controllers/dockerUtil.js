d7df91005394ad812aebf934c394f8c0f5bbe9b2
Aaron_Y <amyerke@uncc.edu>
2019-08-24 17:09:48 -0400

    Files could be comma seperated
    Clean up some console logs

--- web_app/controllers/dockerUtil.js
+++ web_app/controllers/dockerUtil.js
@@ -12,9 +12,6 @@ const HOST_BLJ = process.env.HOST_BLJ;
 
 exports.verifyHostDir = function (req, res, next) {
   try {//source ${BLJ}/script docker_functions ; verifyHostFile /Users/aaronyerke/git/BioLockJ/web_app/Dockerfile
-    console.log('req.pth', req.body.path);
-    console.log(`source ${path.join(bljDir, 'script', 'docker_functions')} ; verifyHostDir ${req.body.path}`);
-    
     let spwn = spawn(`source ${path.join(bljDir, 'script', 'docker_functions')} ; verifyHostDir ${req.body.path}`, {shell: '/bin/bash'});
     spwn.stdout.on('data', function (data) {
       console.log('stdout: ' + data);
